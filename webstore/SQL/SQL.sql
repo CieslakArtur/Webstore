484adac3aad4d917258c8dd0d36f1e99b777be68
CieslakArtur <artur.cieslak1993@wp.pl>
2018-01-21 19:17:17 +0100

    AC

--- webstore/SQL/SQL.sql
+++ webstore/SQL/SQL.sql
@@ -1,53 +1,46 @@
-use webstore;
+CREATE DATABASE `webstore` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) COLLATE utf8_polish_ci NOT NULL,
-  `description` text COLLATE utf8_polish_ci,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_polish_ci,
+  `img` longblob,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
-CREATE TABLE `customer` (
-  `customerId` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) COLLATE utf8_polish_ci NOT NULL,
-  `surname` varchar(45) COLLATE utf8_polish_ci NOT NULL,
-  `address` varchar(200) COLLATE utf8_polish_ci DEFAULT NULL,
-  `email` varchar(45) COLLATE utf8_polish_ci DEFAULT NULL,
-  PRIMARY KEY (`customerId`),
-  UNIQUE KEY `customerId_UNIQUE` (`customerId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `manufacturer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) COLLATE utf32_polish_ci DEFAULT NULL,
+  `name` varchar(45) CHARACTER SET utf32 COLLATE utf32_polish_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf32 COLLATE=utf32_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `productId` int(11) NOT NULL,
-  `customerId` int(11) NOT NULL,
   `unitsInOrder` int(11) NOT NULL DEFAULT '1',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `productId_idx` (`productId`),
-  CONSTRAINT `customerId` FOREIGN KEY (`id`) REFERENCES `customer` (`customerId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `productId` FOREIGN KEY (`id`) REFERENCES `product` (`productId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `customerName` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `customerSurname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `user` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `product` (
   `productId` int(11) NOT NULL AUTO_INCREMENT,
   `categoryId` int(11) NOT NULL,
-  `name` varchar(45) COLLATE utf8_polish_ci NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `unitPrice` decimal(7,2) NOT NULL,
-  `description` text COLLATE utf8_polish_ci,
+  `description` text CHARACTER SET utf8 COLLATE utf8_polish_ci,
   `manufacturerId` int(11) NOT NULL,
   `unitsInStock` int(11) NOT NULL,
-  `discontinued` enum('true','false') COLLATE utf8_polish_ci NOT NULL DEFAULT 'false',
-  `condition` enum('new','used') COLLATE utf8_polish_ci NOT NULL DEFAULT 'new',
+  `discontinued` enum('true','false') CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL DEFAULT 'false',
+  `condition` enum('new','used') CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL DEFAULT 'new',
+  `img` longblob,
   PRIMARY KEY (`productId`),
   KEY `categoryId_idx` (`categoryId`),
   KEY `manufacturerId_idx` (`manufacturerId`),
   CONSTRAINT `categoryId` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `manufacturerId` FOREIGN KEY (`manufacturerId`) REFERENCES `manufacturer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
